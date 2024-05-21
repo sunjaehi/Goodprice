bd5ecb21471e29309e689ea04b38d1794c914cf3
yoon <envy15@skuniv.ac.kr>
2024-05-21 21:55:14 +0900

    feat : 공지사항 페이지 구현

--- src/page/Notice/Notice.jsx
+++ src/page/Notice/Notice.jsx
@@ -1,55 +1,31 @@
-import React from "react";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useEffect, useState } from "react";
 import Pagination from '@mui/material/Pagination';
+import { Container, List, ListItemButton, ListItemText } from "@mui/material";
+import { useSearchParams } from "react-router-dom";
 
-
-function createData(번호, 제목, 글쓴이, 작성날짜) {
-    return { 번호, 제목, 글쓴이, 작성날짜 };
-}
-const rows = [
-    createData('Frozen yogurt',159,6.0,24),
-    createData('Ice cream sandwich',237,9.0,4.3),
-    createData('Eclair',262,16.0,24),
-    createData('Cupcake',305,3.7,67),
-    createData('Gingerbread',365,16.0,49),
-];
 function Notice() {
+    const [notices, setNotices] = useState(null);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const page = searchParams.get('page') || 0;
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/v1/notice/?page=${page}`)
+            .then(response => response.json())
+            .then(json => setNotices(json));
+
+    }, [])
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth:650}} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>번호</TableCell>
-                        <TableCell align="right">제목</TableCell>
-                        <TableCell align="right">글쓴이</TableCell>
-                        <TableCell align="right">작성날짜</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row)=> (
-                        <TableRow
-                            key={row.name}
-                            sx={{'&:last-child td, &:last-child th':{border:0}}}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.번호}
-                            </TableCell>
-                            <TableCell align="right">{row.제목}</TableCell>
-                            <TableCell align="right">{row.글쓴이}</TableCell>
-                            <TableCell align="right">{row.작성날짜}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+        <Container maxWidth="sm">
+            <List>
+                {notices && notices.map(notice => (
+                    <ListItemButton>
+                        <ListItemText
+                            primary={notice.title}
+                            secondary={`조회수 : ${notice.viewCount} - ${notice.author} ${notice.createdAt}`} />
+                    </ListItemButton>
+                ))}
+            </List>
             <Pagination count={10} />
-        </TableContainer>
-        
+        </Container>
     );
 }
 export default Notice;
