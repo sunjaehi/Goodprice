0ccf03037ae4d8f2ece7abd3065065e6356c9b7d
yoon <envy15@skuniv.ac.kr>
2024-05-23 08:58:58 +0900

    feat : 가게추가 백엔드 연동

--- src/admin/page/Registershop.jsx
+++ src/admin/page/Registershop.jsx
@@ -1,216 +1,259 @@
-import React,{useState} from "react";
-import { TextField,Box, MenuItem } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import { TextField, Box, MenuItem } from "@mui/material";
 import Stack from '@mui/material/Stack';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import FormControl from "@mui/material/FormControl";
-import { sectorSample } from "../../data/sectorSample";
 import Button from "@mui/material/Button";
-import Modal from '@mui/material/Modal';
-import { sizing } from '@mui/system';
 import Adminlist from "../section/Adminlist";
 import { useNavigate } from "react-router-dom";
-import DaumPostcode, { useDaumPostcodePopup } from 'react-daum-postcode';
+import { useDaumPostcodePopup } from 'react-daum-postcode';
 import { postcodeScriptUrl } from "react-daum-postcode/lib/loadPostcode";
 import dayjs from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
-
 function Registershop() {
-    const [sector, setSector] = useState('');
+    const [sectors, setSectors] = useState(null);
+    const [sector, setSector] = useState(null);
+    const [zipcode, setZipcode] = useState(null);
+    const [address, setAddress] = useState(null);
+    const [regionId, setRegionId] = useState(null);
+
+    const [startTime, setStartTime] = useState(dayjs('2024-05-20T09:00'));
+    const [endTime, setEndTime] = useState(dayjs());
+    const idInput = useRef(null);
+    const nameInput = useRef(null);
+    const phoneInput = useRef(null);
+    const boastInput = useRef(null);
+    const infoInput = useRef(null);
+
     const handleChange = (e) => {
         setSector(e.target.value);
     }
     const navigate = useNavigate();
-    const [isOpen, setIsOpen] = useState(false);
+    const formData = new FormData();
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/v1/sector/')
+            .then(result => result.json())
+            .then(json => setSectors(json));
+    }, [])
     const open = useDaumPostcodePopup(postcodeScriptUrl);
     const handleComplete = (data) => {
-        let fullAddress = data.address;
-        let extraAddress = ''; //추가될 주소
-        let localAddress = data.sido + ' ' + data.sigungu; //지역주소(시, 도 + 시, 군, 구)
-        if (data.addressType === 'R') { //주소타입이 도로명주소일 경우
-            if (data.bname !== '') {
-                extraAddress += data.bname; //법정동, 법정리
-            }
-        if (data.buildingName !== '') { //건물명
-            extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
-        }
-        //지역주소 제외 전체주소 치환
-        fullAddress = fullAddress.replace(localAddress, '');
-        //조건 판단 완료 후 지역 주소 및 상세주소 state 수정
-        data.setAddressObj({
-            areaAddress: localAddress,
-            townAddress: fullAddress += (extraAddress !== '' ? `(${extraAddress})` : '')
-        });
-        //주소 검색이 완료된 후 결과를 매개변수로 전달
-        //다음에 수행할 작업을 명시
+        if (data.addressType === 'R') {
+            console.log(data);
+            setAddress(data.roadAddress);
+            setZipcode(data.zonecode);
+            setRegionId(data.sigunguCode);
         }
     }
     const handleSearch = () => {
-        open({onComplete:handleComplete});
-        
+        open({ onComplete: handleComplete });
     }
 
+    const handleStartTimeChange = (newValue) => {
+        setStartTime(newValue);
+    };
 
+    const handleEndTimeChange = (newValue) => {
+        setEndTime(newValue);
+    };
 
-    const [value, setValue] = useState(dayjs('2024-05-20T09:00'));
+    const formatTime = (time) => {
+        return time.format('HH:mm');
+    };
+
+    function submit(event) {
+        event.preventDefault();
+        alert(sector);
+        formData.append('id', idInput.current.value);
+        formData.append('name', nameInput.current.value);
+        formData.append('address', address);
+        formData.append('sectorId', sector);
+        formData.append('regionId', regionId);
+        formData.append('phone', phoneInput.current.value);
+        formData.append('boast', boastInput.current.value);
+        formData.append('info', infoInput.current.value);
+        formData.append('businessHours', `${formatTime(startTime)} - ${formatTime(endTime)}`)
+
+        fetch(`http://localhost:8080/api/v1/shop/register`, {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + sessionStorage.getItem("atk")
+            },
+            body: formData,
+        }).then(response => {
+            if (response.status === 200) {
+                alert("등록 성공");
+                navigate('/');
+            }
+            else alert("등록 실패");
+        })
+    }
 
     return (
         <Box
             sx={{
-                flexDirection:"row",
-                display:"flex"
+                flexDirection: "row",
+                display: "flex"
             }}
         >
             <Adminlist />
-        <Stack 
-            component="form"
-            spacing={3}
-            autoComplete="off"
-            width="50%"
-            justifyContent="center"
-            margin={5}
-            ml={10}
-        >
-        <TextField 
-            id="zipcode"
-            label="가게 ID"
-            multiline
-            variant="standard"
-        />
-        <Box
-            sx={{
-                display:"flex",
-                minWidth:"100",
-                flexDirection:"row",
-                gap:3
-            }}
-        >
-        <TextField
-            id="shopname"
-            label="상호명"
-            multiline
-            //maxrows
-            variant="standard"
-            sx={{width:'50%'}}
-        />
-                {/* <InputLabel id="sector">업종별</InputLabel> */}
-                <FormControl sx={{width:'50%'}}>
-                <InputLabel id="select-label">업종 분류</InputLabel>
-                <Select 
-                    labelId="sector-label"
-                    id="sector"
-                    value={sector}
-                    label="업종별"
-                    onChange={handleChange}
-                >
-                    {sectorSample.map((sector)=>(
-                        <MenuItem
-                            value={`${sector.id}`}
-                        >{`${sector.name}`}</MenuItem>
-                    ))}
-                </Select>
-                </FormControl>
-            </Box>
-            <Box
-                sx={{
-                    display:"flex",
-                    minWidth:"100",
-                    flexDirection:"row",
-                }}
-                gap={3}
+            <Stack
+                component="form"
+                spacing={3}
+                autoComplete="off"
+                width="50%"
+                justifyContent="center"
+                margin={5}
+                ml={10}
             >
                 <TextField
-                    id="postcode"
-                    label="우편번호"
+                    id="zipcode"
+                    label="가게 ID"
                     multiline
+                    variant="standard"
+                    inputRef={idInput}
+                />
+                <Box
+                    sx={{
+                        display: "flex",
+                        minWidth: "100",
+                        flexDirection: "row",
+                        gap: 3
+                    }}
+                >
+                    <TextField
+                        id="shopname"
+                        label="상호명"
+                        multiline
+                        //maxrows
+                        variant="standard"
+                        inputRef={nameInput}
+                        sx={{ width: '50%' }}
+                    />
+                    {/* <InputLabel id="sector">업종별</InputLabel> */}
+                    <FormControl sx={{ width: '50%' }}>
+                        <InputLabel id="select-label">업종 분류</InputLabel>
+                        <Select
+                            labelId="sector-label"
+                            id="sector"
+                            value={sector}
+                            label="업종별"
+                            onChange={handleChange}
+                        >
+                            {sectors && sectors.map((sector) => (
+                                <MenuItem
+                                    value={`${sector.id}`}
+                                >{`${sector.name}`}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        minWidth: "100",
+                        flexDirection: "row",
+                    }}
+                    gap={3}
+                >
+                    <TextField
+                        id="postcode"
+                        placeholder="우편번호"
+                        multiline
+                        inputProps={{ readOnly: true, disableUnderline: true }}
+                        value={zipcode}
+                        variant="standard"
+                        sx={{ width: "80%" }}
+                    />
+                    <Button
+                        onClick={handleSearch}
+                        variant="outlined"
+                        color="secondary"
+                    >주소 찾기</Button>
+
+                </Box>
+                <TextField
+                    id="detailAddress"
+                    placeholder="가게 주소"
+                    inputProps={{ readOnly: true, disableUnderline: true }}
+                    multiline
+                    value={address}
                     //maxrows
                     variant="standard"
-                    sx={{width:"80%"}}
-            />
-                <Button
-                    onClick={handleSearch}
-                    variant="outlined"
-                    color="secondary"
-                >주소 찾기</Button>
-                               
-                
-            </Box>
-            <TextField
-                id="detailAddress"
-                label="가게 주소"
-                multiline
-                //maxrows
-                variant="standard"
-            />
-            {/* <DaumPostcode /> */}
-            <TextField
+                />
+                {/* <DaumPostcode /> */}
+                <TextField
                     id="shopPhone"
                     label="연락처"
                     multiline
-                    //maxrows
+                    inputRef={phoneInput}
                     variant="standard"
-            />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DemoContainer components={['TimePicker','TimePicker']}>
-                    <TimePicker
-                        label="시작시간"
-                        defaultValue={dayjs('2024-05-20T09:00')}
-                        sx={{width:'50%'}}
-                    />
-                    <TimePicker
-                        label="종료시간"
-                        value={value}
-                        onChange={(newValue)=>setValue(newValue)}
-                        sx={{width:'50%'}}
-                    />
-                </DemoContainer>
-            </LocalizationProvider>
-            <TextField
+                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <div>
+                        <TimePicker
+                            label="시작시간"
+                            defaultValue={startTime}
+                            onChange={handleStartTimeChange}
+                            sx={{ width: '50%' }}
+                        />
+                        <TimePicker
+                            label="종료시간"
+                            value={endTime}
+                            onChange={handleEndTimeChange}
+                            sx={{ width: '50%' }}
+                        />
+                    </div>
+                    <div>
+                        {`${formatTime(startTime)} - ${formatTime(endTime)}`}
+                    </div>
+                </LocalizationProvider>
+                <TextField
                     id="reason"
                     label="자랑거리"
                     multiline
-                    //maxrows
+                    inputRef={boastInput}
                     variant="standard"
-            />
-            <TextField
+                />
+                <TextField
                     id="info"
                     label="기타정보"
                     multiline
-                    //maxrows
+                    inputRef={infoInput}
                     variant="standard"
-            />
+                />
+                <Box
+                    flexDirection="row"
+                    gap={5}
+                >
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        type="submit"
+                        sx={{
+                            mr: '5px'
+                        }}
+                        onClick={submit}
+                    >등록
+                    </Button>
+                    <Button
+                        variant="contained"
+                    >초기화</Button>
+
+                </Box>
+            </Stack>
             <Box
-                flexDirection="row"
-                gap={5}
+                display="flex"
+                mt={5}
             >
-                <Button 
-                    variant="contained"
-                    color="secondary"
-                    type="submit"
-                    sx={{
-                        mr:'5px'
-                    }}
-                >등록</Button>
-                <Button 
-                    variant="contained"
-                >초기화</Button>
-                    
+                사진 첨부
             </Box>
-        </Stack>
-        <Box 
-            display="flex"
-            mt={5}
-        >
-            사진 첨부
-            
-        </Box>
-    
         </Box>
     );
-        
+
 }
 export default Registershop;
