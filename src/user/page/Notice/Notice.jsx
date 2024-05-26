f6e5f0eec760e64c9699dd735a41cabc6496deef
yoon <envy15@skuniv.ac.kr>
2024-05-26 23:02:08 +0900

    feat : 공지사항 페이징 적용

--- src/user/page/Notice/Notice.jsx
+++ src/user/page/Notice/Notice.jsx
@@ -2,35 +2,56 @@ import React, { useEffect, useState } from "react";
 import Pagination from '@mui/material/Pagination';
 import { Container, List, ListItemButton, ListItemText } from "@mui/material";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { SecurityUpdateGood } from "@mui/icons-material";
 
 function Notice() {
     const navigate = useNavigate();
+    const [response, setResponse] = useState(null);
     const [notices, setNotices] = useState(null);
     const [searchParams, setSearchParams] = useSearchParams();
-    const page = searchParams.get('page') || 1;
+    const [page, setPage] = useState(searchParams.get('page') || 1);
     useEffect(() => {
         fetch(`http://localhost:8080/api/v1/notice/?page=${page - 1}`)
             .then(response => response.json())
-            .then(json => setNotices(json));
+            .then(json => {
+                setResponse(json);
+                if (json && json.notices) {
+                    setNotices(json.notices);
+                } else {
+                    setNotices([]);
+                }
+            });
 
     }, [searchParams])
 
     function handleChange(event, page) {
+        setPage(page);
         navigate(`/Notice?page=${page}`)
     }
     return (
-        <Container maxWidth="sm">
-            <List>
-                {notices && notices.map(notice => (
-                    <ListItemButton component={Link} to={`/NoticeDetail?id=${notice.id}`} key={notice.id}>
-                        <ListItemText
-                            primary={`${notice.title}`}
-                            secondary={`${notice.createdAt} | 조회수 : ${notice.viewCount}`} />
-                    </ListItemButton>
-                ))}
-            </List>
-            <Pagination count={10} onChange={handleChange} />
-        </Container>
+        <>
+            {response &&
+                <Container maxWidth="sm">
+                    <List>
+                        {notices && notices.map(notice => (
+                            <ListItemButton component={Link} to={`/NoticeDetail?id=${notice.id}`} key={notice.id}>
+                                <ListItemText
+                                    primary={`${notice.title}`}
+                                    secondary={`${notice.createdAt} | 조회수 : ${notice.viewCount}`} />
+                            </ListItemButton>
+                        ))}
+                    </List>
+                    <Pagination
+                        count={response.totalPages}
+                        page={page}
+                        onChange={handleChange}
+                        siblingCount={1}
+                        size="large"
+                    />
+                </Container>
+            }
+        </>
     );
+
 }
 export default Notice;
