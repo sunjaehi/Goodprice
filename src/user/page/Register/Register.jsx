d6b74c9779c4f15567329e73aec6d0739335d00a
Lee Sunjae <sunjae7944@naver.com>
2024-06-04 10:09:12 +0900

    feat : 회원가입 stepper

--- src/user/page/Register/Register.jsx
+++ src/user/page/Register/Register.jsx
@@ -1,26 +1,41 @@
-import React, { useState, useRef } from "react";
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { FormControl } from "@mui/material";
-import Alert from '@mui/material/Alert';
-import CheckIcon from '@mui/icons-material/Check';
-import './Register.css';
-import Snackbar from '@mui/material/Snackbar';
-import { useForm } from 'react-hook-form';
+import React, { useState, useRef } from 'react';
+import {
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  Button,
+  Typography,
+  TextField,
+  Box,
+  FormControlLabel,
+  Snackbar,
+  Paper,
+  Grid,
+  Alert,
+  Checkbox
+} from '@mui/material';
 
-function Register() {
-    const theme = createTheme();
+const steps = [
+  {
+    label: '약관 동의',
+    description: '약관에 동의하시면 회원가입이 완료됩니다.',
+  },
+  {
+    label: '이메일 입력',
+    description: '사용하실 이메일을 입력해주세요.',
+  },
+  {
+    label: '닉네임 입력',
+    description: '사용하실 닉네임을 입력해주세요.',
+  },
+  {
+    label: '비밀번호 입력',
+    description: '사용하실 비밀번호를 입력해주세요',
+  },
+];
+
+function StepContentComponent({ step, handleChange, formData }) {
     const [isAvailable, setIsAvailable] = useState(false);
     const [isAvailableEmail, setIsAvailableEmail] = useState(false);
     const [isAvailableNickname, setIsAvailableNickname] = useState(false);
@@ -94,114 +109,167 @@ function Register() {
             </Box>
         </>
     )
+
+  switch (step) {
+    case 0:
+      return (
+        <Box component="form" noValidate onSubmit={handleSubmit}>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={<Checkbox onChange={handleAgree} color="primary" />}
+              label="회원가입에 동의합니다."
+            />
+          </Grid>
+        </Box>
+      );
+    case 1:
+      return (
+        <Box sx={{marginTop:2}}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <TextField
+                autoFocus
+                fullWidth
+                type='email'
+                label='이메일'
+                inputRef={emailInput}
+                name='email'
+                onChange={onEmailFormChange}
+            />
+          </Grid>
+            <Grid item xs={4} sx={{mt:1}}>
+              {button}
+              <Snackbar
+                anchorOrigin={{vertical, horizontal}}
+                open={open}
+                onClose={handleClose}
+                message={isAvailableEmail ? "사용가능한 이메일입니다." : "이미 가입 이력이 있는 이메일입니다."}
+                key={vertical + horizontal}
+              />
+            </Grid>
+          </Grid>
+          
+        </Box>
+      );
+    case 2:
+      return (
+        <Box sx={{marginTop:2}}>
+          <TextField
+            fullWidth
+            id='nickname'
+            name='nickname'
+            label='닉네임'
+          />
+        </Box>
+      );
+    default:
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <PersonAddIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        회원가입
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <FormControl component="fieldset" variant="standard">
-                            <Grid container spacing={2}>
-                                <Grid item xs={8}>
-                                    <TextField
-                                        required
-                                        autoFocus
-                                        fullWidth
-                                        type="email"
-                                        id="email"
-                                        inputRef={emailInput}
-                                        name="email"
-                                        onChange={onEmailFormChange}
-                                        label="이메일 주소"
-                                    />
-                                </Grid>
-                                <Grid item xs={4} sx={{ mt: 1 }}>
-                                    {button}
-                                    <Snackbar
-                                        anchorOrigin={{ vertical, horizontal }}
-                                        open={open}
-                                        onClose={handleClose}
-                                        message={isAvailableEmail ? "사용가능한 이메일입니다" : "이미 가입 이력이 있는 이메일입니다"}
-                                        key={vertical + horizontal}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="nickname"
-                                        name="nickname"
-                                        label="닉네임"
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        label="비밀번호"
-                                        placeholder="숫자+영문자+특수문자 8자리 이상"
-                                        inputRef={passwordInput}
-                                        onChange={onPasswordFormChange}
-                                    />
-                                    {!isAvailabePassword && <Alert severity="warning">숫자,영문자, 특수문자 중 하나 이상을 포함하여 8자리 이상 입력하세요</Alert>}
-                                    {isAvailabePassword && <Alert severity="success">사용 가능한 비밀번호입니다.</Alert>}
-
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        type="password"
-                                        id="password"
-                                        name="rePassword"
-                                        label="비밀번호 확인"
-                                        placeholder="비밀번호를 한 번 더 입력해주세요"
-                                        onChange={onPasswordConfirmFormChange}
-                                        inputRef={passwordConfirmInput}
-                                    />
-                                    {!isAvailableConfirm && <Alert severity="warning">비밀번호가 일치하지 않습니다.</Alert>}
-                                    {isAvailableConfirm && <Alert severity="success">V</Alert>}
-
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <FormControlLabel
-                                        control={<Checkbox onChange={handleAgree} color="primary" />}
-                                        label="회원가입에 동의합니다."
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                color="secondary"
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                                size="large"
-                                disabled={!(isAvailableEmail && isAvailabePassword && isAvailableConfirm)}
-                            >
-                                회원가입
-                            </Button>
-                        </FormControl>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
+      <Box sx={{marginTop:2}}>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            type='password'
+            id='password'
+            name='password'
+            label='비밀번호'
+            placeholder='숫자+영문자+특수문자 8자리 이상'
+            inputRef={passwordInput}
+            onChange={onPasswordFormChange}
+          />
+        {!isAvailabePassword && <Alert severity='warning'>숫자, 영문자, 특수문자 중 하나 이상을 포함하여 8자리 이상을 입력하세요</Alert>}
+        {isAvailabePassword && <Alert severity='success'>사용 가능한 비밀번호입니다.</Alert>}
+      </Grid>
+      </Box>
+      
+      
     );
+  }
 }
+
+function Register() {
+  const [activeStep, setActiveStep] = useState(0);
+  const [formData, setFormData] = useState({
+    email: '',
+    nickname: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  // const handleReset = () => {
+  //   setActiveStep(0);
+  //   setFormData({
+  //     email: '',
+  //     nickname: '',
+  //     password: '',
+  //   });
+  // };
+
+  return (
+    <Box sx={{ width: '50%', margin: 'auto', marginTop: 10 }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel>{step.label}</StepLabel>
+            <StepContent>
+              <Typography>{step.description}</Typography>
+              <StepContentComponent
+                step={index}
+                handleChange={handleChange}
+                formData={formData}
+              />
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1,
+                      // bgcolor:'grey',
+                      // color:'black',
+                      // ":hover" : {
+                      //   bgcolor:"lightgray"
+                      // }
+                    }}
+                  >
+                    {index === steps.length - 1 ? '완료' : '다음'}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    이전
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>버튼을 눌러 회원가입을 완료하세요.</Typography>
+          <Button sx={{ mt: 1, mr: 1 }}>
+            회원가입 완료
+          </Button>
+        </Paper>
+      )}
+    </Box>
+  );
+}
+
 export default Register;
