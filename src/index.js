5c96c918d9462645da443b5ffc93c2a7e7449644
Lee Sunjae <sunjae7944@naver.com>
2024-06-07 16:18:00 +0900

    feat : 글씨체 변경

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { ThemeProvider, createTheme } from '@mui/material';
 
 const theme=createTheme({
   typography: {
-    fontFamily: 'NanumGothicLight'
+    fontFamily: 'NanumGothic'
   }
 
 })
